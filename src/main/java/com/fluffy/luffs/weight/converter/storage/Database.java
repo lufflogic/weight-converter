aa61b6bb2b39e8c50619f4f55dc4c9aae5c20a00
chrislluff <chris1976@me.com>
2022-10-30 16:29:19 +0000

    Db uplift to h2

--- src/main/java/com/fluffy/luffs/weight/converter/storage/Database.java
+++ src/main/java/com/fluffy/luffs/weight/converter/storage/Database.java
@@ -38,10 +38,6 @@ import java.sql.Statement;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 import java.util.function.Function;
 import java.util.logging.Level;
 import java.util.logging.Logger;
